34b7bbf611ab898b9fbce6de8cd2742ef9e1afa8
Audun Follegg <audunfo@gmail.com>
2016-03-29 01:42:28 +0200

    Add eventBus for async communication between components

---
+++ app/src/main/java/no/picklepick/flickerswipe/common/events/EventBus.java
@@ -0,0 +1,21 @@
+package no.picklepick.flickerswipe.common.events;
+
+import com.squareup.otto.Bus;
+import com.squareup.otto.ThreadEnforcer;
+
+/**
+ * Simple singleton holder for Otto {@link Bus}.
+ *
+ * @author Audun Follegg (audunfo@gmail.com)
+ */
+public class EventBus {
+
+    public static Bus eventBus;
+
+    public static Bus getEventBus() {
+        if(eventBus == null){
+            eventBus = new Bus(ThreadEnforcer.ANY);
+        }
+        return eventBus;
+    }
+}
