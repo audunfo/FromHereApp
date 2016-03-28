244f20747da4627fd09b7bb6bf15144a418aef06
Audun Follegg <audunfo@gmail.com>
2016-03-29 01:57:14 +0200

    Update location endpoint with appropriate params

--- app/src/main/java/no/picklepick/flickerswipe/api/instagram/endpoints/LocationEndpoint.java
+++ app/src/main/java/no/picklepick/flickerswipe/api/instagram/endpoints/LocationEndpoint.java
@@ -1,7 +1,12 @@
 package no.picklepick.flickerswipe.api.instagram.endpoints;
 
+import no.picklepick.flickerswipe.api.instagram.model.ListResponse;
+import no.picklepick.flickerswipe.api.instagram.model.location.InstagramLocation;
+import no.picklepick.flickerswipe.api.instagram.model.media.Media;
 import retrofit.Call;
 import retrofit.http.GET;
+import retrofit.http.Path;
+import retrofit.http.Query;
 
 /**
  * Retrofit service definition for Instagram.
@@ -12,10 +17,12 @@ public interface LocationEndpoint {
 
 
     @GET("locations/search")
-    Call<?> searchLocation();
+    Call<ListResponse<InstagramLocation>> searchLocations(@Query("lat") double latitude,
+                                                          @Query("lng") double longitude,
+                                                          @Query("access_token") String accessToken);
 
     @GET("locations/{locationId}/media/recent")
-    Call<?> getRecentImagesForLocation();
+    Call<ListResponse<Media>> getRecentImagesForLocation(@Path("locationId") Long locationId, @Query("access_token") String accessToken);
 
 
 
