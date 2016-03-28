71eb0413cf5484274923589d0d90a01ad8024e2a
Audun Follegg <audunfo@gmail.com>
2016-03-29 01:47:02 +0200

    Add base activity to handle registration of eventbus and showing dialogs

---
+++ app/src/main/java/no/picklepick/flickerswipe/common/BaseActivity.java
@@ -0,0 +1,52 @@
+package no.picklepick.flickerswipe.common;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+
+import no.picklepick.flickerswipe.common.dialogs.DialogManager;
+import no.picklepick.flickerswipe.common.events.EventBus;
+
+/**
+ * Common activity functionality.
+ *
+ * @author Audun Follegg (audunfo@gmail.com)
+ */
+public class BaseActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EventBus.getEventBus().register(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventBus.getEventBus().unregister(this);
+    }
+
+    protected void showSuccessDialog(String title){
+        final MaterialDialog successDialog = DialogManager.getSuccessDialogWithTitle(this, title).show();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                successDialog.dismiss();
+            }
+        }, 1700);
+    }
+
+    protected void showErrorDialog(String title){
+        final MaterialDialog errorDialog = DialogManager.getErrorDialogWithTitle(this, title).show();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                errorDialog.dismiss();
+            }
+        }, 1700);
+    }
+}
